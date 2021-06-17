6a93d44ab40d511cad10d27e7fbcfed5281e0ec2
Sarah Vaupel <sarah.vaupel@protonmail.com>
2021-06-17 15:42:04 +0200

    enhance coc settings

--- neovim/default.nix
+++ neovim/default.nix
@@ -1,16 +1,25 @@
 { pkgs, ... }:
+let
+  coc = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      coc-nvim
+      coc-git
+    ];
+    settings = builtins.readFile ./coc/basic/settings.json;
+  };
+in
 {
   programs.neovim = {
     enable = true;
     vimAlias = true;
     plugins = with pkgs.vimPlugins; [
       airline
-      coc-nvim
-      coc-git
       fugitive
       vim-nix
-    ];
+    ] // if coc.enable then coc.plugins else [];
     extraConfig = builtins.readFile ../.vimrc;
   };
-  home.file.".config/nvim/coc-settings.json".text = builtins.readFile ./coc/basic/settings.json;  # TODO: move to coc.nix
-}
+} // if coc.enable then {
+  home.file.".config/nvim/coc-settings.json".text = coc.settings;
+} else {}
